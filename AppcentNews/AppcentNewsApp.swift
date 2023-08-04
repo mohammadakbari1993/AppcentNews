a53c7c6628afc614c15ce232d64d0c8cde449a4c
Mohammad Akbari <mohammad.a@nizek.com>
2023-08-04 11:20:50 +0330

    Initial Commit

---
+++ AppcentNews/AppcentNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppcentNewsApp.swift
+//  AppcentNews
+//
+//  Created by Mohammad Akbari on 5/13/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct AppcentNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
